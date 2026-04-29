6be2e9efad2ecb9547a738459ca3983c4cfa0a15
Ameer Hamza <ameerhamza9356@outlook.com>
2026-04-29 22:03:44 +0500

    files

---
+++ animated-toggle-button/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const demoButton = document.getElementById('demoBtn');
+
+  demoButton.addEventListener('click', () => {
+    // Replace this with your actual logic, like redirecting to a form
+    alert('Taking you to the order page!');
+  });
+});
